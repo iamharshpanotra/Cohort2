e9853b186580800883285e0912652f846cb1ab8d
Harsh <harsh.sharma@maxworthsystems.com>
2025-04-11 11:25:31 +0530

    Function to find Sum of two numbers

---
+++ Codes/findSum.js
@@ -0,0 +1,8 @@
+function findSum(a,b) {
+    //Goal of a function: It does things with the input and returns the output
+    const sumValue = a + b;
+    return sumValue;
+}
+
+const sum = findSum(5,10);
+console.log(sum);
