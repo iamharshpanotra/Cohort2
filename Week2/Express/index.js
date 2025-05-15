30cd27a16b4ac9de196322608b31dea4ee6d7f1d
Harsh <harsh.sharma@maxworthsystems.com>
2025-05-15 11:45:46 +0530

    Creating a simple HTTP server using Express

--- Week2/Express/index.js
+++ Week2/Express/index.js
@@ -2,3 +2,20 @@
 // node default library => no
 
 const express = require('express');  
+
+const app = express();
+
+function sum(n){
+    let ans = 0;
+    for (let i = 1; i<=n; i++){
+        ans = ans + i; 
+    }
+    return ans;
+}
+
+app.get("/", function(req, res){
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("Hi Nikhil, you're doing great! The sum of first " + n + " numbers is: " + ans);
+})
+app.listen(3000);
