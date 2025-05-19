546b55d70b71ba42d89879768e2ebdd28e587bba
Harsh <harsh.sharma@maxworthsystems.com>
2025-05-19 23:43:54 +0530

    Middlewares

--- Week3/Codes/inputValidationCorrectWay.js
+++ Week3/Codes/inputValidationCorrectWay.js
@@ -4,25 +4,16 @@ const app = express();
 
 const port = 3000;
 
-app.get("/health-checkup", function (req, res){
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
-
-    if (username != "mehra" || password != "amazon"){
-        res.status(400).json({"msg": "Something wrong with the inputs"})
-        return
-    }
-    if(kidneyId != 1 && kidneyId != 2){
-        res.status(400).json({"msg": "Something wrong with the inputs"})
-        return
-    }
-
-    res.json({
-        msg: "Your kidney is fine!"
-    })
+app.get("/health-checkup", function (req, res, next){
+    console.log("Hi, from first!");
+    next();
+}, function(req, res){
+    console.log("Hi from Second!");
+    res.send("New here?")
 });
 
 
- 
+
+
+
 app.listen(port);
